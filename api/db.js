c7761c898645171129db7f4e87ef6bee97d0161d
Symon Peter Tubon <dev.symon@gmail.com>
2024-03-17 20:24:36 +0800

    Add time and project models

--- api/db.js
+++ api/db.js
@@ -25,7 +25,69 @@ const User = sequelize.define('User', {
     allowNull: false,
   },
 });
+
+
+const Time = sequelize.define('Time', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  start: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  end: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+})
+
+const Project = sequelize.define('Project', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+})
+
+/** Relations */
+
+// User
+User.hasMany(Time)
+
+// Project
+Project.hasMany(time)
+
+// Time
+Time.belongsTo(User)
+Time.belongsTo(Project)
+
+/** Init Data */
+
+const projectData = [
+  { name: 'Project 1' },
+  { name: 'Project 2' },
+  { name: 'Project 3' },
+  // Add more projects as needed
+];
+
+// Create and save instances of the Project model
+Project.bulkCreate(projectData)
+  .then(() => {
+    console.log('Projects inserted successfully');
+  })
+  .catch(err => {
+    console.error('Error inserting projects:', err);
+  });
+
 module.exports = {
   sequelize,
-  User
+  User,
+  Time,
+  Project
 }
