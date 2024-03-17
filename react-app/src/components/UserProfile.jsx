5e9d43c118fd784fda53ee834a98996ce0dee4d8
Symon Peter Tubon <dev.symon@gmail.com>
2024-03-17 19:54:56 +0800

    Add template user profile

--- react-app/src/components/UserProfile.jsx
+++ react-app/src/components/UserProfile.jsx
@@ -1,6 +1,16 @@
+import { Box, Button, Typography } from "@mui/material"
+
 const UserProfile = () => {
     return (
-        <>UserProfile</>
+        <Box display={'flex'} flexDirection={'column'} alignItems={'center'} gap={2}>
+            <img src="https://placebacon.net/150/150" alt="" style={{borderRadius: '50%', border: '3px solid grey'}}/>
+            <Typography variant="h6">
+                Template Name
+            </Typography>
+            <Typography>
+                Template Title - Software Dev
+            </Typography>
+        </Box>
     )
 }
 
